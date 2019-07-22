61802fcd3542f2d78a40f8b325f3571d81df2935
dikshasuvarna <dikshasuvarna855@gmail.com>
2019-07-22 10:54:45 +0530

    initial commit

---
+++ dice.py
@@ -0,0 +1,19 @@
+''' dice application'''
+import random as rn
+num=rn.randint(1,6)
+count=0
+while True:
+    inp_num=int(input("enter the number:"))
+    if inp_num==num:
+        count+=1
+        print(f"you guessed number in {count} attempts")
+        break
+    elif inp_num<num:
+        print("guessed number is less than actual number:")
+        count+=1
+    else:
+        print("guessed number is more than actual number:")
+        count+=1
+    if count==3:
+        print("better luck next time")
+        break         
